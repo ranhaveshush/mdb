961668b8058ecb1f5878bafcdec844c0d93c5330
Ran Haveshush <ran.haveshush.dev@gmail.com>
2019-05-30 11:21:11 +0200

    Use appcompat toolbar.

--- app/src/main/java/com/ranhaveshush/mdb/view/MainActivity.kt
+++ app/src/main/java/com/ranhaveshush/mdb/view/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.ranhaveshush.mdb
+package com.ranhaveshush.mdb.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ranhaveshush.mdb.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
