d9cbf838fc88fd23e5e9ee5f3f2e5c55738d1dd5
Ran Haveshush <ran.haveshush.dev@gmail.com>
2019-08-14 12:34:57 +0200

    Add Coil - Coroutine Image Loader dependency.

--- app/src/main/java/com/ranhaveshush/mdb/api/tmdb/TmdbApi.kt
+++ app/src/main/java/com/ranhaveshush/mdb/api/tmdb/TmdbApi.kt
@@ -10,7 +10,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 /**
  * A [TMDb](https://developers.themoviedb.org/3/) client API implementation.
  *
- * The Movie Database (TMDb) is a community built MoviesViewModelmovie and TV database.
+ * The Movie Database (TMDb) is a community built movies and TV database.
  * Every piece of data has been added by our amazing community dating back to 2008.
  * TMDb's strong international focus and breadth of data is largely unmatched and something we're incredibly proud of.
  * Put simply, we live and breathe community and that's precisely what makes us different.
